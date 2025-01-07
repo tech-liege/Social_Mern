24ae192c0f1db4186c8a1f4ad7238bb2241a9da9
Daniel Idowu <danielidowu938@gmail.com>
2025-01-07 12:47:10 +0100

    Still Day 3 A bbeak

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
   try {
-    hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(req.query.password, 10);
     // const user = await User.create(req.body);
     // res.status(201).json(user);
     const user = new User({
-      username: req.body.username,
-      email: req.body.email,
+      username: req.query.username,
+      email: req.query.email,
       password: hashedPassword,
     });
     const newUser = await user.save();
@@ -20,6 +21,32 @@ router.post('/register', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ 'email': req.query.email });
+    if (!user) return res.status(404).json({ message: 'User does not exist' });
+
+    const isMatch = await bcrypt.compare(req.query.password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET); //, { expiresIn: '1h' });
+
+    res.header('Authorization', token).json({ token: token });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.post('/profile', async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+ 
 
 // // Get all users
 // router.get('/', async (req, res) => {
