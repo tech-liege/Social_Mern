ecfb473047a7e901104ef327fe2bc1534e3f6409
Daniel Idowu <danielidowu938@gmail.com>
2025-01-09 11:43:19 +0100

    Everything In Place - Let's get coding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//import component
+import Home from './Components/Home/Home.jsx';
+import Profile from './Components/Profile/Profile.jsx';
+import Login from './Components/Auth/Login.jsx';
+import Register from './Components/Auth/Register.jsx';
+import Explore from './Components/Explore/Explore.jsx';
+import Feed from './Components/Feed/Feed.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Posts from './Components/Posts/Posts.jsx';
+import CreatePost from './Components/Posts/CreatePost.jsx';
+import UserProfile from './Components/UserProfile/UserProfile.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/feed' element={<Feed />} />
+            <Route path='/explore' element={<Explore />} />
+            <Route path='/posts' element={<Posts />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/user/:userId' element={<UserProfile />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/create' element={<CreatePost />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
